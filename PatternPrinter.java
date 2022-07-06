35c17d86086f1b71105603fc248835f7057b94b4
lav <kumarlav59@gmail.com>
2022-07-06 16:48:32 +0700

    Assignment Q2

---
+++ PatternPrinter.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class PatternPrinter {
+ public static void main(String[] args) {
+ Scanner in = new Scanner(System.in);
+ int n = in .nextInt();
+ patternPrinter(n);
+ }
+ static void patternPrinter(int n) {
+ // write your code here
+ 	int in=n;
+ 	while(n>0){
+ 		for(int j=in;j>0;j--)
+ 			for(int i=1;i<=n;i++)
+ 			{
+				System.out.print(j+" ");
+			}
+ 		System.out.println();
+ 		n--;
+ 	}
+
+ }
+}
